6770616798cef523117d0baa11bb47d9698635c8
ogre <ogre@jbadigital.com>
2013-09-07 16:56:56 +1000

    stowin' symlinks in install.sh

--- install.sh
+++ install.sh
@@ -3,85 +3,68 @@
 usage="Usage: ./install.sh [-f force] app1 [app2 app3 ..]"
 
 FORCE=0
-
 while getopts "f" options
 do
 	case $options in
 		f ) FORCE=1;;
-		\? ) echo $usage
-			 exit 1;;
 		* ) echo $usage
 			exit 1;;
 	esac
 done
+shift $((OPTIND-1))
 
-function create_sym() {
-	if [ -h "$2" ]; then
-		# if symlink exists, replace it
-		ln -fs "$1" "$2"
-
-	elif [ -f "$2" ]; then
-		if [ $FORCE -eq 1 ]; then
-			# replace existing file
-			ln -fs "$1" "$2"
-		else
-			# if a file exists, ask user
-			echo "Do you want to delete the existing config file '$2'? [y/N]"
-			read y
-			if [ "$y" == "y" ]; then
-				ln -fs "$1" "$2"
-			fi
-		fi
-	else
-		# doesnt exist, so create
-		ln -s "$1" "$2"
-	fi
-}
-
-function create_dir() {
-	# Recurse from $1 creating symlinks to all files in $HOME
-	# Any hidden dirs in $1 will be recursed creating BOTH dir
-	# and symlinking their descendant children
-	for ITEM in $(find "$1" -mindepth 1 -maxdepth 1 -name \.\*)
-	do
-		LN=${ITEM#*$1/}
-		if [ -d "$1/$LN" ]; then
-			# recurse directories symlinking everything below
-			mkdir -p "$HOME/$LN"
-			for ITEM2 in $(find "$1/$LN" -mindepth 1)
-			do
-				LN2=${ITEM2#*$LN/}
-				if [ -d "$1/$LN/$LN2" ]; then
-					mkdir -p "$HOME/$LN/$LN2"
-				elif [ -f "$1/$LN/$LN2" ]; then
-					# create symlinks
-					create_sym "$1/$LN/$LN2" "$HOME/$LN/$LN2"
-				fi
-			done
-		elif [ -f "$1/$LN" ]; then
-			# create symlinks
-			create_sym "$1/$LN" "$HOME/$LN"
-		fi
-	done
-}
+# TODO verify execution is one dir down from $HOME
+# ie, $HOME/dotfiles/install.sh
 
 PWD=$(pwd)
 
 for app in "$@"
 do
-	if [ -d "$PWD/$app" ]; then
-		# create a directory and all containing symlinks
-		create_dir "$PWD/$app"
+	# if -f is supplied forcibly overwrite existing files
+	if [ $FORCE -eq 1 ]; then
+		IFS=$(echo -en "\n\b")
 
-	elif [ -f "$PWD/.$app" ]; then
-		# create a symlink
-		create_sym "$PWD/${SRC[$i]}" "$HOME/${DST[$i]}"
+		# check stow version
+		VERSION=$(stow -V | awk '/stow/ {print $5}')
+		if [ ${VERSION:0:1} -eq 1 ]
+		then
+			# v.1.3.3
+			# parse stow's conficts
+			CONFLICTS=$(stow -c $app 2>&1 | awk '/CONFLICT/ {print $4}')
+			for filename in $CONFLICTS; do
+				if [ -f $filename ]; then
+					echo "Deleting symlink $filename"
+					rm -f "$filename"
+				fi
+			done
+		else
+			# v2.2.0
+			# remove existing symlinks
+			CONFLICTS=$(stow -nv --stow $app 2>&1 | awk '/not owned by stow/ {print $9}')
+			for filename in $CONFLICTS; do
+				if [ -L "$HOME/$filename" ]; then
+					echo "Deleting symlink $HOME/$filename"
+					rm -f "$HOME/$filename"
+				fi
+			done
+
+			# remove files which would be replaced with symlinks
+			CONFLICTS=$(stow -nv --stow $app 2>&1 | awk '/neither a link nor a directory/ {print $11}')
+			for filename in $CONFLICTS; do
+				if [ -f "$HOME/$filename" ]; then
+					echo "Deleting file $HOME/$filename"
+					rm -f "$HOME/$filename"
+				fi
+			done
+		fi
 	fi
+
+	# use stow to create symlinks in $HOME
+	stow -v --restow $app
 
 	# special behaviour for vim and vundle
 	if [ "$app" == "vim" ]; then
 		git submodule update --init
-		create_dir "$PWD/$app"
 		vim +BundleInstall +qall
 	fi
 done
