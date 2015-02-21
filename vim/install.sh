e554b38013b9b108b393a9d939f404d48617324e
mafro <github@mafro.net>
2015-02-22 09:56:48 +1100

    vim: redirect output on Vundle install if TTY missing

--- vim/install.sh
+++ vim/install.sh
@@ -5,5 +5,10 @@ git submodule update --init vim/.vim/bundle/Vundle.vim
 
 # install vim plugins with Vundle
 if [[ $? -eq 0 ]]; then
-	vim +PluginInstall +qall
+	# if either stdin or stdout is missing, redirect output
+	if [[ ! -t 0 || ! -t 1 ]]; then
+		vim +PluginInstall +qall &>/dev/null
+	else
+		vim +PluginInstall +qall
+	fi
 fi
