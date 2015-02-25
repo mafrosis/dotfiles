772de7cbac66b91e673264c47c712695e2ae5afc
mafro <github@mafro.net>
2015-02-26 07:45:14 +1100

    salt: rtorrent: improved move-torrent.sh

--- salt/rtorrent/move-torrent.sh
+++ salt/rtorrent/move-torrent.sh
@@ -1,42 +1,54 @@
 #! /bin/bash
 
-# rtorrent passes d.get_custom1 in $1
-#  and d.get_base_path in $2
+# rtorrent passes d.get_custom1 in $1 and d.get_base_path in $2
 
-usage="move-torrent.sh [-h] tag download_path"
+USAGE='move-torrent.sh [-h] label download_path'
 
-while getopts "h" options
+while getopts 'h' options
 do
 	case $options in
-		h ) echo $usage
-			exit 1;;
-		\? ) echo $usage
-			 exit 1;;
-		* ) echo $usage
-			exit 1;;
+		h ) echo $USAGE && exit 1;;
 	esac
 done
 shift $((OPTIND-1))
 
-if [ $# -ne 2 ]; then
+# label arrives urlencoded from rtorrent
+LABEL=$(python -c "import urllib; print urllib.unquote('$1')")
+SOURCE_PATH=$2
+
+if [[ $# -ne 2 ]]; then
 	echo $usage
 	exit 1
 fi
 
+if [[ -z $LABEL ]]; then
+	echo 'No tag specified'
+	exit 1
+fi
+
 # verify download_path is valid
-if [ ! -d "$2" ] && [ ! -f "$2" ]; then
-	echo "Download target is neither file or directory"
+if [[ ! -d $SOURCE_PATH && ! -f $SOURCE_PATH ]]; then
+	echo 'Download target is neither file or directory'
 	exit 2
 fi
 
-if [ "$1" == "movie" ]; then
-	DEST="/media/pools/video/Movies/HD-1080p"
-elif [ "$1" == "music" ]; then
-	DEST="/media/pools/music/mp3/DJ"
+# set custom destination paths for some special labels
+if [[ $LABEL == 'movie' ]]; then
+	DEST='/media/pools/video/Movies/HD-1080p'
+elif [[ $LABEL == 'music' ]]; then
+	DEST='/media/pools/music/mp3/DJ'
 else
-	DEST="/media/pools/video/$1"
+	DEST="/media/pools/video/$LABEL"
 fi
 
-rsync -avP "$2" "$DEST"
+# copy the download to its destination
+rsync -avP "$SOURCE_PATH" "$DEST"
+
+if [[ $? -eq 0 ]]; then
+	echo "$SOURCE_PATH :: $DEST" >> /var/log/move-torrent.log
+else
+	echo "$SOURCE_PATH :: FAILED TO COPY ($?)" >> /var/log/move-torrent.log
+fi
+
+# set permissions on the destination folder
 find "$DEST" -type d -exec sudo chown mafro:video {} \;
-echo "$2 :: $DEST" >> /var/log/move-torrent.log
