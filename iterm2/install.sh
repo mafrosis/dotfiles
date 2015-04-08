741f036803f20ab86e240eeab8a52cf5ee647ed3
mafro <github@mafro.net>
2015-04-08 13:22:45 +1000

    iterm2: install Monaco font and abort

---
+++ iterm2/install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# install Powerline font
+cp iterm2/Monaco-Powerline.otf /Library/Fonts
+
+echo "You will need to set the 'Load preferences from a custom folder' option in iterm" 
+
+# skip stow in top-level install.sh
+exit 255
