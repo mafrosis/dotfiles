cf19b67a1afe67e7a689c70bfcd52177196b4500
mafro <github@mafro.net>
2015-04-09 07:42:44 +1000

    git: install git package in install.sh

--- git/install.sh
+++ git/install.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+if [[ -z $(which git) ]]; then
+
+	# install git package
+	if [[ $(uname) == 'Darwin' ]]; then
+		if [[ -z $(which brew) ]] ; then
+			echo 'Run ./install.sh osx first to bootstrap OSX with Homebrew'
+			exit 3
+		fi
+		brew install git
+
+	elif [[ $(uname) == 'Linux' ]]; then
+		sudo aptitude install git
+	fi
+
+fi
+
 # create bin directory in $HOME before stow symlinks into it
 if [[ ! -d $HOME/bin ]]; then
 	mkdir $HOME/bin
