689763acb52d43f7cf04617189f61d6e9ae69e06
mafro <github@mafro.net>
2015-04-09 07:42:55 +1000

    zsh: install ZSH package in install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
+if [[ -z $(which zsh) ]]; then
+
+	# install ZSH package
+	if [[ $(uname) == 'Darwin' ]]; then
+		if [[ -z $(which brew) ]] ; then
+			echo 'Run ./install.sh osx first to bootstrap OSX with Homebrew'
+			exit 3
+		fi
+		brew install zsh
+
+	elif [[ $(uname) == 'Linux' ]]; then
+		sudo aptitude install zsh
+	fi
+
+fi
+
 # update prezto submodule
 git submodule update --init --recursive prezto
 
