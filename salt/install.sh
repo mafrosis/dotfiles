a3a9ed58a4f815e5d9ecefe5879aa01c927c745e
mafro <github@mafro.net>
2015-04-10 08:57:16 +1000

    salt: bootstrap salt-minion with install.sh

---
+++ salt/install.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# install salt package
+if [[ $(uname) == 'Darwin' ]]; then
+	if [[ -z $(which brew) ]] ; then
+		echo 'Run ./install.sh osx first to bootstrap OSX with Homebrew'
+		exit 3
+	fi
+	brew install saltstack
+
+	# configure just the salt-minion (no salt-master is setup)
+	if [[ ! -d /etc/salt ]]; then
+		sudo mkdir /etc/salt
+	fi
+
+	sudo touch /etc/salt/minion
+	sudo tee /etc/salt/minion > /dev/null <<EOF
+file_client: local
+id: $(hostname -s)
+file_roots:
+  base:
+    - $HOME/dotfiles/salt
+    - $HOME/dotfiles/salt-formulae
+pillar_roots:
+  base:
+    - $HOME/dotfiles/salt/pillar
+EOF
+
+	# print the install salt version
+	salt-call --version
+
+elif [[ $(uname) == 'Linux' ]]; then
+	# install salt-minion via bootstrap
+	curl -L http://bootstrap.saltstack.org | sudo sh -s -- git v2014.7.4
+fi
+
+# skip stow in top-level install.sh
+exit 255
