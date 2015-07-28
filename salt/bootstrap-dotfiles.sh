9a71631871c9bbdd04a82e780ad6238f53f4b418
Matt Black <github@mafro.net>
2015-07-28 12:56:46 +0100

    bootstrap/pillar: switch to sesame.sh

--- salt/bootstrap-dotfiles.sh
+++ salt/bootstrap-dotfiles.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-USAGE="bootstrap-dotfiles.sh -k sesame-key [-v salt-tagname] [-b git-branch]"
+USAGE="bootstrap-dotfiles.sh [-p sesame-password] [-v salt-tagname] [-b git-branch]"
 
-SESAME_KEY=''
-SALT_VERSION='v2014.1.13'
+SESAME_PASS=''
+SALT_VERSION='v2015.5.3'
 BRANCH=''
 
 red='\033[0;91m'
 green='\033[0;92m'
 reset='\033[0m'
 
-while getopts "h:k:v:b:" options
+while getopts "h:p:v:b:" options
 do
 	case $options in
-		k ) SESAME_KEY=$OPTARG;;
+		p ) SESAME_PASS=$OPTARG;;
 		v ) SALT_VERSION=$OPTARG;;
 		b ) BRANCH=$OPTARG;;
 		* ) echo "$USAGE"
@@ -27,8 +27,8 @@ if [[ ! $(id -u) -eq 0 ]]; then
 	exit 1
 fi
 
-if [[ ! -f $SESAME_KEY ]]; then
-	echo -en "${red}You must include a path to the sesame key for the encrypted pillar${reset}\n  $USAGE"
+if [[ -z $SESAME_PASS ]]; then
+	echo -en "${red}You must the sesame password for the encrypted pillar${reset}\n  $USAGE"
 	exit 1
 fi
 
@@ -37,10 +37,10 @@ if ! command -v git >/dev/null 2>&1; then
 	apt-get install -y curl git
 fi
 
-# install python-pip && sesame
-if ! command -v sesame >/dev/null 2>&1; then
-	apt-get install -y python-dev python-pip
-	pip install sesame
+# install sesame.sh
+if ! command -v sesame.sh >/dev/null 2>&1; then
+	curl -o /usr/local/bin/sesame.sh https://raw.githubusercontent.com/mafrosis/sesame.sh/master/sesame.sh
+	chmod +x /usr/local/bin/sesame.sh
 fi
 
 # install salt
@@ -78,15 +78,13 @@ EOF
 # decrypt the pillar file with sesame
 if [[ ! -f /home/mafro/dotfiles/salt/pillar/$(hostname -s)-secrets.sls ]]; then
 	cd /home/mafro/dotfiles/salt/pillar
-	sesame decrypt -f -k "$SESAME_KEY" "$(hostname -s)-secrets.enc"
+	sesame.sh d -f -p "$SESAME_PASS" "$(hostname -s)-secrets.sesame"
 	echo 'Pillar decrypted'
 
 	if [[ $? -gt 0 ]]; then
 		echo -e "${red}Failed decrypting sensitive pillar!${reset}"
 		exit 3
 	fi
-
-	cp "$SESAME_KEY" /home/mafro/dotfiles/salt/pillar
 fi
 
 # fix permissions
