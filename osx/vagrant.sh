919036b0e487ed278d9f39bec9f0c528bd857994
Matt Black <github@mafro.net>
2015-09-14 09:31:45 +0100

    osx: vagrant.sh: recreate networking every time

--- osx/vagrant.sh
+++ osx/vagrant.sh
@@ -22,10 +22,20 @@ else
 	echo 'Vagrant VMWare license not found at ~/Desktop/license.lic'
 fi
 
+# stop VMWare networking and recreate base config
+sudo ~/Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --stop
+sudo rm -rf /Library/Preferences/VMware\ Fusion/networking* /Library/Preferences/VMware\ Fusion/vmnet*
+sudo ~/Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --configure
+
 # configure VMWare vmnet devices to nice IP ranges
 for N in 1 8; do
 	sudo sed -i '' -e "s/.*VNET_${N}_HOSTONLY_SUBNET.*/answer VNET_${N}_HOSTONLY_SUBNET 172.16.${N}.0/" /Library/Preferences/VMware\ Fusion/networking
 done
+
+# notify if VMWare restart necessary
+if pgrep -f vmware-fusion; then
+	echo 'You must restart VMWare Fusion'
+fi
 
 # also install Packer
 brew install packer
