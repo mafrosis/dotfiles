84fd686462061aadc2e3e78df9d9cb2c74c33615
Matt Black <github@mafro.net>
2016-03-16 13:04:34 +0000

    osx: script improvements ala shellcheck

--- osx/install.sh
+++ osx/install.sh
@@ -12,7 +12,7 @@ if [[ $2 -eq 1 ]]; then DRY_RUN='-n'; else DRY_RUN=''; fi
 # setup OSX defaults; sudo is required
 sudo -v
 if [[ $? -eq 0 ]]; then
-	$HOME/dotfiles/osx/set-defaults.sh
+	./set-defaults.sh
 	echo 'You will probably need to restart OSX to make new settings take effect'
 else
 	echo 'Sudo failed, skipping OSX defaults'
@@ -21,7 +21,7 @@ fi
 
 # create bin directory in $HOME before stow symlinks into it
 if [[ ! -d $HOME/bin ]]; then
-	mkdir $HOME/bin
+	mkdir "$HOME/bin"
 fi
 
 # retrieve the morgant/tools-osx submodule
@@ -29,11 +29,12 @@ git submodule update --init osx/tools-osx
 
 # stow some handy scripts from morgant/tools-osx into ~/bin
 # add more by symlinking them into dotfiles/osx/bin
-cd $HOME/dotfiles/osx
-stow -v bin $RESTOW --target=$HOME/bin $DRY_RUN
+cd "$HOME/dotfiles/osx" || exit 2
+stow -v bin $RESTOW --target="$HOME/bin" "$DRY_RUN"
 
 
 # install Homebrew and apps
+# shellcheck disable=SC1091
 source ./homebrew.sh
 
 
