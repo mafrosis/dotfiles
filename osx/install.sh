9f2258c5ddb73374773009568df2a46be3576506
Matt Black <github@mafro.net>
2016-09-16 20:09:02 +0100

    osx: set defaults after installing everything else

--- osx/install.sh
+++ osx/install.sh
@@ -9,16 +9,6 @@ if [[ $1 -eq 1 ]]; then RESTOW='--restow'; else RESTOW=''; fi
 if [[ $2 -eq 1 ]]; then DRY_RUN='-n'; else DRY_RUN=''; fi
 
 
-# setup OSX defaults; sudo is required
-sudo -v
-if [[ $? -eq 0 ]]; then
-	./set-defaults.sh
-	echo 'You will probably need to restart OSX to make new settings take effect'
-else
-	echo 'Sudo failed, skipping OSX defaults'
-fi
-
-
 # create bin directory in $HOME before stow symlinks into it
 if [[ ! -d $HOME/bin ]]; then
 	mkdir "$HOME/bin"
@@ -36,6 +26,17 @@ stow -v bin $RESTOW --target="$HOME/bin" "$DRY_RUN"
 # install Homebrew and apps
 # shellcheck disable=SC1091
 source ./homebrew.sh
+
+
+# setup OSX defaults; sudo is required
+sudo -v
+if [[ $? -eq 0 ]]; then
+	# shellcheck disable=SC1091
+	source set-defaults.sh
+	echo 'You will need to restart to make new macOS defaults take effect'
+else
+	echo 'Sudo failed, skipping OSX defaults'
+fi
 
 
 # skip stow in top-level install.sh
