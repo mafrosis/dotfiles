3eb94fb5ebdedf344e79d557bd356e867e4ef412
Matt Black <github@mafro.net>
2016-09-17 10:37:16 +0100

    iterm2: switch to customised Hack font, plus config

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-# install Powerline font
-cp iterm2/Monaco-Powerline.otf /Library/Fonts
+# install customised version of Hack font
+cp iterm2/HackMafro-Regular.otf /Library/Fonts
 
 echo "You will need to set the 'Load preferences from a custom folder' option in iterm" 
 
