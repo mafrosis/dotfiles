87dbc2c858b3343bd73e0fc382d973c311d4c127
Matt Black <github@mafro.net>
2019-03-19 08:47:05 +1100

    mp3: sync-music.sh: bi-directional sync script

--- salt/mp3/sync-music.sh
+++ salt/mp3/sync-music.sh
@@ -1,18 +1,43 @@
-#! /bin/bash
+#! /bin/bash -e
 
-echo "Setting owner and mode on /media/pools/music"
+NAS_HOSTNAME=${NAS_HOSTNAME:-jorg}
+NAS_USERNAME=${NAS_USERNAME:-mafro}
+MPD_HOSTNAME=${MPD_HOSTNAME:-kvothe}
+MPD_USERNAME=${MPD_USERNAME:-pi}
 
-# set ownership on source
-sudo chown mafro:audio -R /media/pools/music
+if command -v mpd &>/dev/null; then
+	# mpd exists; script running on MPD host
 
-# set permissions on source
-sudo find /media/pools/music -type d -exec chmod 750 {} \;
-sudo find /media/pools/music -type f -exec chmod 640 {} \;
+	echo "Syncing from $NAS_USERNAME@$NAS_HOSTNAME"
 
-echo "Syncing to Kerplunk"
+	# pull music from NAS
+	rsync -avP --delete "$NAS_HOSTNAME:/media/pools/music/mp3/" "$HOME/mp3"
 
-# pull playlists from kerplunk
-rsync -avP --delete kerplunk:/home/mafro/playlists/ /media/pools/music/playlists
+	# set ownership on mpd directory
+	sudo chown "$MPD_USERNAME:audio" -R "$HOME/mp3"
 
-# push music to kerplunk
-rsync -avP --delete /media/pools/music/mp3/ kerplunk:/home/mafro/mp3
+	# set permissions on mpd directory
+	sudo find "$HOME/mp3" -type d -exec chmod 750 {} \;
+	sudo find "$HOME/mp3" -type f -exec chmod 640 {} \;
+
+else
+	# mpd doesn't exist; script running on NAS
+
+	# push music to MPD server
+	echo 'Setting owner and mode on /media/pools/music'
+
+	# set ownership on source
+	sudo chown "$NAS_USERNAME:audio" -R /media/pools/music
+
+	# set permissions on source
+	sudo find /media/pools/music -type d -exec chmod 750 {} \;
+	sudo find /media/pools/music -type f -exec chmod 640 {} \;
+
+	echo "Syncing to $MPD_USERNAME@$MPD_HOSTNAME"
+
+	# pull playlists from the MPD server
+	rsync -avP --delete "$MPD_HOSTNAME:/home/$MPD_USERNAME/playlists/" /media/pools/music/playlists
+
+	# push music to the MPD server
+	rsync -avP --delete /media/pools/music/mp3/ "$MPD_HOSTNAME:/home/$MPD_USERNAME/mp3"
+fi
