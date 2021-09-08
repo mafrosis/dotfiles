108ddf1f210f3a3b10df988d5fcc13e50bf4baf2
Matt Black <github@mafro.net>
2021-09-08 21:53:33 +1000

    tmux: cleanup and simplify

--- tmux/install.sh
+++ tmux/install.sh
@@ -22,23 +22,25 @@ fi
 # symlink all custom themes into tmux-powerline source
 for F in tmux/powerline-themes/*; do
 	echo "Installing powerline theme $F"
-	ln -sf "$HOME/dotfiles/$F" "$HOME/tmux-powerline/themes/"
+	ln -sf "$HOME/dotfiles/$F" "tmux/tmux-powerline/themes/"
 done
-
-tee tmux/.tmux-powerlinerc > /dev/null <<EOF
-export TMUX_POWERLINE_DEBUG_MODE_ENABLED=false
-export TMUX_POWERLINE_PATCHED_FONT_IN_USE=true
-export TMUX_POWERLINE_THEME=generic
-EOF
-
-# if this host has a custom theme, switch to it
-if [[ -f "tmux/powerline-themes/$(hostname).sh" ]]; then
-	echo "Switching to theme $(hostname).sh"
-	sed -i "s/THEME=generic/THEME=$(hostname)/g" tmux/.tmux-powerlinerc
-fi
 
 # symlink all custom segments into tmux-powerline source
 for F in tmux/powerline-segments/*; do
 	echo "Installing powerline segment $F"
-	ln -sf "$HOME/dotfiles/$F" "$HOME/tmux-powerline/segments/"
+	ln -sf "$HOME/dotfiles/$F" "tmux/tmux-powerline/segments/"
 done
+
+THEME=generic
+
+# if this host has a custom theme, switch to it
+if [[ -f "tmux/powerline-themes/$(hostname).sh" ]]; then
+	echo "Switching to theme $(hostname).sh"
+	THEME="$(hostname)"
+fi
+
+tee "$HOME/.tmux-powerlinerc" > /dev/null <<EOF
+export TMUX_POWERLINE_DEBUG_MODE_ENABLED=false
+export TMUX_POWERLINE_PATCHED_FONT_IN_USE=true
+export TMUX_POWERLINE_THEME=${THEME}
+EOF
