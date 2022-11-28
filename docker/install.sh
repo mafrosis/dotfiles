9f110e788e1eed92fd67f7e6468228355620046e
Matt Black <github@mafro.net>
2022-11-28 16:35:32 +1100

    docker: Improved install.sh for linux

--- docker/install.sh
+++ docker/install.sh
@@ -12,7 +12,15 @@ if ! command -v docker >/dev/null 2>&1; then
 		brew install homebrew/cask/docker
 
 	elif [[ $(uname) == 'Linux' ]]; then
-		curl -fsSL https://get.docker.com | sudo bash
+		# add docker APT repo
+		curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+		# install docker & compose
+		sudo apt-get update && sudo apt-get install -y docker-ce docker-compose-plugin
+
+		# add user to the docker group
+		sudo usermod -aG docker $(whoami)
 	fi
 fi
 
