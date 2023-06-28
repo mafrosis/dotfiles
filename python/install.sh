30be8e37eb48f5c52fb8c8b17a4c8e71f4fce421
Matt Black <github@mafro.net>
2023-06-28 17:07:22 +1000

    python: rewrite install script for python3 and ensurepip

--- python/install.sh
+++ python/install.sh
@@ -1,73 +1,31 @@
-#! /bin/bash -e
-
-# Never install python2
-# If python2 is already installed, install pip for python2
-# Always ensure python3+pip is installed
+#! /bin/zsh -e
 
 # DEBUG mode controlled by env var
 if [[ -n $DEBUG ]]; then set -x; fi
 
-# Assume nothing is installed
-PYTHON2=0
-PYTHON3=0
-PYTHON2_PIP=0
-PYTHON3_PIP=0
-
-# Check python versions available
-if command -v python2 >/dev/null 2>&1; then
-	PYTHON2=1
-	if command -v python2 -m pip -V >/dev/null 2>&1; then
-		PYTHON2_PIP=1
-	fi
-fi
-
-if command -v python3 >/dev/null 2>&1; then
-	PYTHON3=1
-	if command -v python3 -m pip -V >/dev/null 2>&1; then
-		PYTHON3_PIP=1
-	fi
-fi
-
-if command -v python >/dev/null 2>&1; then
-	if [[ $(python -V 2>&1) =~ (.*)2\.(.*) ]]; then
-		PYTHON2=1
-		if command -v python -m pip -V >/dev/null 2>&1; then
-			PYTHON2_PIP=1
-		fi
-	fi
-	if [[ $(python -V 2>&1) =~ (.*)3\.(.*) ]]; then
-		PYTHON3=1
-		if command -v python -m pip -V >/dev/null 2>&1; then
-			PYTHON3_PIP=1
-		fi
-	fi
-fi
-
-function print_installed() {
-	[[ $1 -eq 1 ]] && echo "$2 is already installed" || echo "$2 is not installed"
+function info {
+	>&2 print "\e[32m$1\e[0m"
 }
-print_installed $PYTHON2 'Python 2'
-print_installed $PYTHON3 'Python 3'
-print_installed $PYTHON2_PIP 'Pip for python 2'
-print_installed $PYTHON3_PIP 'Pip for python 3'
 
-if [[ $(uname) == 'Darwin' ]]; then
-	if [ $PYTHON3 -eq 0 ]; then
+# Install python3 package if missing
+if ! command -v python3 >/dev/null 2>&1; then
+	info '## Installing python3'
+
+	if [[ $(uname) == 'Darwin' ]]; then
 		brew install python3
-	fi
 
-elif [[ $(uname) == 'Linux' ]]; then
-	if [ $PYTHON3 -eq 0 ]; then
-		sudo apt-get install -y python3 python3-dev
+	elif [[ $(uname) == 'Linux' ]]; then
+		sudo apt-get install -y python3 python3-dev pipx
 	fi
 fi
 
-if [ $PYTHON3_PIP -eq 0 ]; then
-	curl -L https://bootstrap.pypa.io/get-pip.py | python3
+# Ensure python symlink is present
+if ! command -v python >/dev/null 2>&1; then
+	info '## Symlinking python3'
+	sudo ln -sf $(which python3) /usr/local/bin/python
 fi
 
-if [ $PYTHON2 -eq 1 ] && [ $PYTHON2_PIP -eq 0 ]; then
-	curl -L https://bootstrap.pypa.io/pip/2.7/get-pip.py | python
-	ln -s "$HOME/Library/Python/2.7/bin/pip2" /usr/local/bin
-fi
-echo 'Done'
+info '## Ensuring & upgrading pip'
+python -m ensurepip --upgrade
+
+info '## fin'
