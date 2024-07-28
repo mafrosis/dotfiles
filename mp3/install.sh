648996714c617ee6ed3f1240fdf4622eff4bcd36
Matt Black <github@mafro.net>
2024-07-28 12:54:29 +1000

    mp3: audio encoding tools & eyeD3

---
+++ mp3/install.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -e
+
+# DEBUG mode controlled by env var
+if [[ -n $DEBUG ]]; then set -x; fi
+
+function info {
+	>&2 print "\e[32m$1\e[0m"
+}
+
+requires=(lame flac opus-tools)
+
+for app in $requires; do
+	if ! command -v lame >/dev/null 2>&1; then
+		info "## Installing ${app}"
+
+		if [[ $(uname) == 'Darwin' ]]; then
+			brew install ${app}
+
+		elif [[ $(uname) == 'Linux' ]]; then
+			sudo apt-get install -y ${app}
+		fi
+	fi
+done
+
+info '## Installing eyeD3 globally'
+pipx install eyeD3
