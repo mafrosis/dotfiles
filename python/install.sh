0d3369b526bc98f6c47578f88d3a64948d6e0de7
Matt Black <github@mafro.net>
2024-09-25 13:18:13 +0100

    fixup! 1ae1088

--- python/install.sh
+++ python/install.sh
@@ -12,16 +12,10 @@ if ! command -v python3 >/dev/null 2>&1; then
 	info '## Installing python3'
 
 	if [[ $(uname) == 'Darwin' ]]; then
-		if [[ $PYTHON3 -eq 0 ]]; then
-			brew install python3 pipx
-			PYTHON3=1
-		fi
+		brew install python3 pipx
 
 	elif [[ $(uname) == 'Linux' ]]; then
-		if [[ $PYTHON3 -eq 0 ]]; then
-			sudo apt-get install -y python3 python3-dev pipx
-			PYTHON3=1
-		fi
+		sudo apt-get install -y python3 python3-dev pipx
 	fi
 fi
 
