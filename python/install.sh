e21031b15adad613e778ff94f660541c18221891
Matt Black <github@mafro.net>
2025-04-25 12:12:54 +1000

    python: Replace pipx/hatch with uv

--- python/install.sh
+++ python/install.sh
@@ -8,21 +8,26 @@ function info {
 }
 
 if [[ -n $TERMUX_VERSION ]]; then
-	pkg install -y python3
-	pip install pipx
+	pkg install -y python3 uv
 
 elif [[ $(uname) == 'Darwin' ]]; then
 	# On macos, need to replace system python with Homebrew
 	if python3 --help | grep /Library/Developer/CommandLineTools; then
 		info '## Installing python3 & pipx'
-		brew install python3 pipx
+		brew install python3
+	fi
+	if ! command -v uv >/dev/null 2>&1; then
+		brew install uv
 	fi
 
 elif [[ $(uname) == 'Linux' ]]; then
 	# Install python3 package if missing
 	if ! command -v python3 >/dev/null 2>&1; then
 		info '## Installing python3 & pipx'
-		sudo apt-get install -y python3 python3-dev pipx
+		sudo apt-get install -y python3 python3-dev
+	fi
+	if ! command -v uv >/dev/null 2>&1; then
+		curl -LsSf https://astral.sh/uv/install.sh | sh
 	fi
 fi
 
@@ -33,6 +38,4 @@ if ! command -v python >/dev/null 2>&1; then
 fi
 
 info '## Install hatch'
-pipx install hatch
-
-info '## fin'
+uv install hatch
