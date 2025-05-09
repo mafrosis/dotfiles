1698438f16b506415048c47cff699f23ff8c5475
Matt Black <github@mafro.net>
2025-05-09 12:35:41 +1000

    iterm2: Updated to new Hack nerd font

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -3,8 +3,12 @@
 # DEBUG mode controlled by env var
 if [[ -n $DEBUG ]]; then set -x; fi
 
-# install customised version of Hack font
-cp iterm2/HackMafro-Regular.otf /Library/Fonts
+# Custom font
+if [[ ! -f ~/Library/Fonts/HackNerdFont-Regular.ttf ]]; then
+	curl -L -o /tmp/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Hack.zip
+	unzip -o -d /tmp /tmp/Hack.zip
+	cp /tmp/HackNerdFont-Regular.ttf ~/Library/Fonts
+fi
 
 echo "You will need to set the 'Load preferences from a custom folder' option in iterm" 
 
