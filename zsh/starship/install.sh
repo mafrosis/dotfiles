9123b3a1e2768c7771d13279754ff8e167405354
Matt Black <github@mafro.net>
2025-05-13 21:08:49 +1000

    zsh: Add starship to replace prezto prompt

---
+++ zsh/starship/install.sh
@@ -0,0 +1,20 @@
+#! /bin/zsh -e
+
+source ./lib.sh
+info '## Setup starship'
+
+# Install starship package
+if [[ $FORCE -eq 0 ]] && command -v starship >/dev/null 2>&1; then
+	echo 'starship already installed!'
+else
+	if [[ -n $TERMUX_VERSION ]]; then
+		pkg install getconf
+		curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir /data/data/com.termux/files/usr/bin
+
+	elif [[ $(uname) == 'Linux' ]]; then
+		sudo apt install -y starship
+
+	elif [[ $(uname) == 'Darwin' ]]; then
+		brew install starship
+	fi
+fi
