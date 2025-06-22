5000ccaf636256090b0471738063faebc1d6ad6d
Matt Black <github@mafro.net>
2025-06-23 06:30:15 +1000

    llm: Bootstrap for simonw's llm CLI

--- llm/install.sh
+++ llm/install.sh
@@ -1,15 +1,35 @@
 #! /bin/zsh
 
+source ./lib.sh
+
+info 'Installing llm.datasette.io'
+
 # https://llm.datasette.io
 uv tool install llm
 
 # Add openrouter
-llm install llm-openrouter
-llm keys set openrouter --value $OPENROUTER_API_KEY
-echo "$(llm models list | grep openrouter | wc -l) models available"
+if ! llm plugins | grep -q llm-openrouter; then
+	llm install llm-openrouter
+fi
+if [[ -n $OPENROUTER_KEY ]]; then
+	llm keys set openrouter --value $OPENROUTER_API_KEY
+	echo "$(llm models list | grep openrouter | wc -l) models available"
+else
+	warn 'Missing $OPENROUTER_KEY, not configured for LLM'
+fi
 
 # Add ollama
-llm install llm-ollama
+if ! llm plugins | grep -q llm-ollama; then
+	llm install llm-ollama
+fi
 
 # More plugins
-llm install llm-jq
+if ! llm plugins | grep -q llm-jq; then
+	llm install llm-jq
+fi
+if ! llm plugins | grep -q llm-cmd; then
+	llm install llm-cmd
+fi
+
+echo 'Plugins:'
+llm plugins | grep name
