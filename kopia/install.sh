1df4abc48cede2210a13cba68ec09fb58533b569
Matt Black <github@mafro.net>
2025-07-12 07:53:57 +1000

    fixup! 97602e2

--- kopia/install.sh
+++ kopia/install.sh
@@ -16,3 +16,27 @@ if ! command -v kopia >/dev/null 2>&1; then
 		sudo apt-get update && sudo apt-get install -y kopia
 	fi
 fi
+
+mkdir -p $HOME/.config/kopia
+tee $HOME/.config/kopia/repository.config > /dev/null <<EOF
+{
+  "storage": {
+    "type": "gcs",
+    "config": {
+      "bucket": "mafro-homelab-backup",
+      "credentialsFile": "${HOME}/.config/kopia/mafro-backup-7deca97f9b3e.json"
+    }
+  },
+  "caching": {
+    "cacheDirectory": "${HOME}/.cache/kopia/29dc260a5e3b849a",
+    "maxCacheSize": 5242880000,
+    "maxMetadataCacheSize": 5242880000,
+    "maxListCacheDuration": 30
+  },
+  "hostname": "$(hostname)",
+  "username": "mafro",
+  "description": "Repository in GCS: mafro-homelab-backup",
+  "enableActions": false,
+  "formatBlobCacheDuration": 900000000000
+}
+EOF
